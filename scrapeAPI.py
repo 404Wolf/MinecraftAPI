615b702ea5e6857d2fbd2a87587419afbdd2d559
Wolf <wolf@techy.cc>
2021-11-06 18:14:10 -0400

    API and discord bot work at same time

--- scrapeAPI.py
+++ scrapeAPI.py
@@ -4,20 +4,20 @@ import discord
 import sys
 import json
 
-with open("config.json") as config:
-	config = json.load(config)
-	
-with open("chats.json") as config:
-	config = json.load(config)
+with open("config.json") as configFile:
+	config = json.load(configFile)
+
+with open("chats.json") as chatsFile:
+	chats = json.load(chatsFile)
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-	print("Authed ["+client.id+"]")
-	msg = await (client.get_channel(log_channel)).send("**Ember Helper** has logged on.")
-	game = discord.Game(f"Sniping names")
+	print("Bot has authed")
+	game = discord.Game("Scraping NameMC")
 	await client.change_presence(status=discord.Status.online, activity=game)
+	asyncio.create_task(api())
 
 async def api():
 	routes = web.RouteTableDef()
@@ -25,16 +25,16 @@ async def api():
 	@routes.get('/')
 	async def ping(request):
 		print(request.host)
-		return web.Response(text="Hello World")
+		return web.Response(text="API for scraping NameMC data")
 
 	async def startServer():
 		app = web.Application()
 		app.add_routes(routes)
-
 		runner = web.AppRunner(app)
 		await runner.setup()
 		site = web.TCPSite(runner,host="0.0.0.0",port=5000)
 		await site.start()
+		print("API has booted")
 
 	asyncio.create_task(startServer())
 
@@ -44,4 +44,4 @@ async def api():
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-client.run(token)
+client.run(config['token'])
