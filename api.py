55141109e2f99604073481263946ba9b968209b5
Wolf <wolf@techy.cc>
2021-11-07 22:24:46 -0500

    stable commit w/o bugs

--- api.py
+++ api.py
@@ -3,21 +3,16 @@ from aiohttp import web
 import discord
 import sys
 import json
-from random import randint
-from time import mktime,time
 from random import randint,choice
 from time import mktime,time,sleep
 from datetime import datetime
 import socket
-
 with open("config.json") as configFile:
 	config = json.load(configFile)
-
 client = discord.Client()
 cache = {}
 current = 0
 ratelimit = 0
-
 @client.event
 async def on_ready():
 	global channels
@@ -35,51 +30,23 @@ async def on_ready():
 			await channel.delete()
 		for i in range(100):
 			channels.append(await client.get_guild(config["server"]).create_text_channel("scraper-"+str(i+1)))
-
 async def ratelimitReset():
 	global ratelimit
 	while True:
 		ratelimit = 0
-		await asyncio.sleep(5)
+		await asyncio.sleep(10)
+		await asyncio.sleep(60)
 
 async def lookup(target):
 	global channels
 	global current
 	data = ""
 	while len(str(data)) < 10:
-		tag = str(randint(int("1"*20),int("9"*20)))
-		channel = await client.get_guild(config["server"]).create_text_channel(target)
 		try:
-			await channel.send("https://namemc.com/search?q="+target+"&c="+tag)
-			while True:
-				try:
-					target = target
-					data = {"target":target}
-					async for message in channel.history(limit=1):
-						messageFromHist = message
-					raw = messageFromHist.embeds[0].description
-					data["searches"] = int(raw[raw.find("Searches: "):].replace("Searches: ","").replace(" / month",""))
-					if "Availability" in raw:
-						droptime = mktime(datetime.strptime(raw[:raw.find("Z,")].replace("Time of Availability: ",""), "%Y-%m-%dT%H:%M:%S").timetuple())
-						data["status"] = "dropping"
-						data["droptime"] = droptime
-					elif "Unavailable" in raw:
-						data["status"] = "unavailable"
-						data["droptime"] = None
-					elif "Available*" in raw:
-						data["status"] = "available"
-						data["droptime"] = None
-					elif "Invalid" or "Too Short" in raw:
-						data["status"] = "invalid"
-						data["droptime"] = None
-					break
-				except IndexError:
-					await asyncio.sleep(.05)
 			channel = channels[current]
 		except IndexError:
 			current = 0
 		finally:
-			asyncio.create_task(channel.delete())
 			current += 1
 		async def send():
 			await channel.send("https://namemc.com/search?q="+target+"&c="+str(randint(int("1"*20),int("9"*20))))
@@ -114,17 +81,14 @@ async def lookup(target):
 				await asyncio.sleep(.075)
 				attempts += 1
 	return data
-
 async def api():
 	routes = web.RouteTableDef()
 	@routes.get('/')
 	async def api_ping(request):
 		return web.Response(text="API for scraping NameMC data")
-
 	@routes.get("/lookup")
 	async def api_lookup(request):
 		global ratelimit
-
 		try:
 			target = request.headers["target"]
 		except KeyError:
@@ -134,20 +98,18 @@ async def api():
 		target = target.lower()
 
 		if (target not in cache) or (cache[target]["recheck"] < time()):
-			data = await lookup(target)
-			if ratelimit > 5:
-				return web.json_response({"target":target,"error":"Ratelimit hit! Please try again in 20-30 seconds."})
-			data = await asyncio.wait_for(lookup(target),3)
-			cache[target] = {}
-			cache[target]["data"] = data
-			cache[target]["recheck"] = time()+60*5
-			cache[target]["recheck"] = time()+60*10
-			ratelimit += 1
-			return web.json_response(data)
-
-		return web.json_response(cache[target]["data"],indent=3)
+			try:
+				if ratelimit > 20:
+					return web.json_response({"target":target,"error":"Ratelimit hit! Please try again in 20-30 seconds."})
+				data = await asyncio.wait_for(lookup(target),3)
+				cache[target] = {}
+				cache[target]["data"] = data
+				cache[target]["recheck"] = time()+60*10
+				ratelimit += 1
+				return web.json_response(data)
+			except:
+				return web.Response(text="Make sure to include a target!\nYou can pass it in the query string (?target=name), or as a header ({\"target\":\"name\"})")
 		return web.json_response(cache[target]["data"])
-
 	async def startServer():
 		app = web.Application()
 		app.add_routes(routes)
